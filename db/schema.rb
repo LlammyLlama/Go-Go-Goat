c0e3bcd35cab9cd536da9bf3785c42c381e7234e
Emily <emilymeiyilam@gmail.com>
2022-12-01 21:41:31 +1100

    updated user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_100018) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_095258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "status"
+    t.date "rental_start_date"
+    t.date "rental_end_date"
+    t.string "service"
+    t.bigint "goat_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["goat_id"], name: "index_bookings_on_goat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "goats", force: :cascade do |t|
     t.string "name"
@@ -23,6 +36,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_100018) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_goats_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "user_id"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +63,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_100018) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "goats"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "goats", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
