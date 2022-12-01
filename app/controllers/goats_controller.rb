3060b66ae4e167401bc689348b207ed25d870342
Stephanie Calderon <sncalderon3@gmail.com>
2022-12-01 20:48:11 +1100

    created a new method and creat method

--- app/controllers/goats_controller.rb
+++ app/controllers/goats_controller.rb
@@ -1,2 +1,23 @@
 class GoatsController < ApplicationController
+  def index
+  end
+
+  def new
+    @goat = Goat.new
+  end
+
+  def create
+    @goat = Goat.new(goat_params)
+    if @goat.save
+      redirect_to goat_path(@goat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def goat_params
+    params.require(:goat).permit(:name, :age, :services, :rate_per_day, :appetite, :description, :image)
+  end
 end
