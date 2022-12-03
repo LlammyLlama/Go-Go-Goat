fd53426bc9f5be82910164acef03388229183078
Stephanie Calderon <sncalderon3@gmail.com>
2022-12-03 14:03:20 +1100

    updated controller pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @goats = Goat.all
     @user = current_user
+    @goats = Goat.all
   end
 
 end
