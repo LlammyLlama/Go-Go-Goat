318b9579b63d26f7877f30cee966a8a3d5c39db6
Emily <emilymeiyilam@gmail.com>
2022-12-03 13:04:35 +1100

    bookings done, sans date picking calendar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,14 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @goat = Goat.find(params[:goat_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    @booking.goat = Goat.find(params[:goat_id])
+    @booking.status = "pending"
     if @booking.save
       redirect_to root_path # need to change this to the dashboard path
     else
@@ -19,11 +22,7 @@ class BookingsController < ApplicationController
 
   private
 
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
   def booking_params
-    params.require(:booking).permit(:status, :rental_start_date, :rental_end_date, :service)
+    params.require(:booking).permit(:rental_start_date, :rental_end_date)
   end
 end
