17d11a47662c39193e17f05ea4ade5807c5c6140
Emily <emilymeiyilam@gmail.com>
2022-12-03 11:32:33 +1100

    uploading current app - half baked bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,6 +8,22 @@ class BookingsController < ApplicationController
   end
 
   def create
-    
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to root_path # need to change this to the dashboard path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:status, :rental_start_date, :rental_end_date, :service)
   end
 end
