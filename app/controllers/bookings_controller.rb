9b08c851070e4214cebce80f73ed5b1d161b4878
Ben Treglown <treggy89@gmail.com>
2022-12-03 14:02:41 +1100

    ignore these

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,34 @@
 class BookingsController < ApplicationController
   def index
+    @bookinbs = Booking.all
+  end
+
+  def new
+    @booking = Bookings.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to root_path # need to take to dashboard
+    else
+      render :new, status: :unprocessable_entity
+
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_goat
+    @goat
+  end
+
+  def booking_params
+    params.require(:booking).permit(:status, :rental_start_date, :rental_end_date, :service)
   end
 end
