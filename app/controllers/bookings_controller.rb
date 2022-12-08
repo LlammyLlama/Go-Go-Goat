268b0bd3a4366fa635ed4043348f764f455dd0f1
Ben Treglown <treggy89@gmail.com>
2022-12-08 14:42:23 +1100

    Did small changes to goat img on show page and fixed aligning of goat cards on the home page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,5 @@
 class BookingsController < ApplicationController
   def index
-<<<<<<< HEAD
-    @bookinbs = Booking.all
-  end
-
-  def new
-    @booking = Bookings.new
-=======
     @bookings = Booking.all
   end
 
@@ -14,45 +7,23 @@ class BookingsController < ApplicationController
     @user = current_user
     @booking = Booking.new
     @goat = Goat.find(params[:goat_id])
->>>>>>> c4c8813ed7e97a01675d704e7b4c78043895df34
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
-<<<<<<< HEAD
-    if @booking.save
-      redirect_to root_path # need to take to dashboard
-    else
-      render :new, status: :unprocessable_entity
-
-=======
     @booking.goat = Goat.find(params[:goat_id])
     @booking.status = "pending"
     if @booking.save
       redirect_to dashboard_path(@booking.user)
     else
       render :new, status: :unprocessable_entity
->>>>>>> c4c8813ed7e97a01675d704e7b4c78043895df34
     end
   end
 
   private
 
-<<<<<<< HEAD
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def set_goat
-    @goat
-  end
-
-  def booking_params
-    params.require(:booking).permit(:status, :rental_start_date, :rental_end_date, :service)
-=======
   def booking_params
     params.require(:booking).permit(:rental_start_date, :rental_end_date)
->>>>>>> c4c8813ed7e97a01675d704e7b4c78043895df34
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-<<<<<<< HEAD
-=======
   get "/users/:id/dashboard", to: "users#show", as: :dashboard
   # resources :users, only: [:show]
->>>>>>> c4c8813ed7e97a01675d704e7b4c78043895df34
   resources :goats do
     resources :bookings, only: %i[new create]
   end
