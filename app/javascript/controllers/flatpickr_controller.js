06d7e83bad941d6db3fa3ce79d332f4e92f0673b
Stephanie Calderon <sncalderon3@gmail.com>
2022-12-10 10:25:54 +1100

    calendar is set to always-open

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+// Import the rangePlugin from the flatpickr library
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = [ "rentalStartDate", "rentalEndDate" ]
+  connect() {
+    console.log('Flatpickr')
+    const startFlatpickrInstance = flatpickr(this.rentalStartDateTarget, {
+                                      // Provide an id for the plugin to work
+                                      // "plugins": [new rangePlugin({ input: "#booking_rental_end_date"})]
+                                      closeOnSelect: false,
+                                      altInput: true,
+                                      altFormat: "F j, Y",
+                                      dateFormat: "m-d-Y",
+                                      inline: true
+                                    })
+    // startFlatpickrInstance.open() // opens datepickr
+    flatpickr(this.rentalEndDateTarget, {})
+  }
+}
