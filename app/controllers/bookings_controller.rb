2648789aae0ad85b2e8d9b7e5ddc7e48db041240
Emily Lam <32960484+LlammyLlama@users.noreply.github.com>
2022-12-10 15:39:48 +1100

    Revert "last changes"

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -28,12 +28,10 @@ class BookingsController < ApplicationController
 
   def approve
     @booking.update(status: "Approved")
-    redirect_to dashboard_path
   end
 
   def deny
     @booking.update(status: "Denied")
-    redirect_to dashboard_path
   end
 
   def edit
